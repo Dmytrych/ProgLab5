25e5fe7e71c078bbc574b1efd7aeea47ab7a521c
Bohdan <misterrtwister@gmail.com>
2020-06-06 00:26:50 +0300

    fix setter, upgrade tree and final work

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -7,11 +7,42 @@ namespace TreesLab
 {
     class Program
     {
+        //                          123*(a/(43-t))-5*b^(f/e^k)-45/f
+        //                          (15^a-b*3)/((4^(d-c))*c-a*d)
+
         static void Main(string[] args)
         {
-
+            string problem = Console.ReadLine();
+            string s = "notNullString";
+            Hashtable table = new Hashtable();
+            Tree tree = new Tree(table);
+            Node pointer = tree.topNode;
+            while (s != "")
+            {
+                s = Console.ReadLine();
+                if (s != "")
+                {
+                    s = s.Replace("=", " ");
+                    s = s.Replace(";", " ");
+                    string[] parts = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    Node setter = new Node(table, Node.Operation.Setter, "=", tree.topNode);
+                    Node a = new Node(table, Node.Operation.Data, parts[0], setter);
+                    Node b = new Node(table, Node.Operation.Data, parts[1], setter);
+                    setter.Nodes.Add(a);
+                    setter.Nodes.Add(b);
+                    pointer.Nodes.Add(setter);
+                }
+            }
+            problem = CreatePostfixNotation(problem);
+            Console.WriteLine(problem);
+            tree.topNode.BuildTree(problem);
+            Console.WriteLine(tree.Calculate());
+            Console.WriteLine(table["a"]);
+            Console.WriteLine(table["b"]);
+            Console.WriteLine(table["c"]);
+            Console.WriteLine(table["d"]);
         }
-        static string CreatePostfixNotation(string input)
+        public static string CreatePostfixNotation(string input)
         {
             input = input.Replace("+", " + ");
             input = input.Replace("-", " - ");
@@ -98,7 +129,8 @@ namespace TreesLab
                             if (stack.Peek() == "^" || stack.Peek() == "/" || stack.Peek() == "*" || stack.Peek() == "-" || stack.Peek() == "+" || stack.Peek() == "!")
                             {
                                 result += " ";
-                                result += stack.Pop().ToString(); result += " ";
+                                result += stack.Pop().ToString();
+                                result += " ";
                             }
                             else
                             {
@@ -112,7 +144,7 @@ namespace TreesLab
                         break;
                     case ")":
                         while (stack.Peek() != "(")
-                        {
+                        { 
                             result += " ";
                             result += stack.Pop().ToString();
                         }
--- Lab5/Lab5/Tree.cs
+++ Lab5/Lab5/Tree.cs
@@ -5,94 +5,122 @@ using System.Text;
 
 namespace TreesLab
 {
- 
-        public class Node
+    public class Tree
+    {
+        public Node topNode;
+        Hashtable table;
+        public Tree(Hashtable hashtable)
         {
+            this.table = hashtable;
+            this.topNode = new Node(hashtable, Node.Operation.Empty, "", null);
+        }
+
+        public double Calculate()
+        {
+            return Convert.ToDouble(topNode.Visit());
+        }
+    }
+
+    public class Node
+    {
         Hashtable hashtable;
         Node parent;
         int type;
         string data;
         public enum Operation
-            {
-                Operator, Setter, Data, Empty
-            }
-            public Node(Hashtable hashtable, Operation op, string data, Node parent)
-            {
+        {
+            Operator, Setter, Data, Empty
+        }
+        public Node(Hashtable hashtable, Operation op, string data, Node parent)
+        {
             this.type = (int)op;
             this.hashtable = hashtable;
             this.parent = parent;
             this.data = data;
             Nodes = new List<Node>();
-            }
-           public List<Node> Nodes { get; set; }
+        }
+        public List<Node> Nodes { get; set; }
 
-            public string Visit()
+        public string Visit()
+        {
+            switch (this.type)
             {
-                switch (this.type)
-                {
-                    case (int)Operation.Operator:
-                        double a = Convert.ToDouble(Nodes[0].Visit());
-                        double b = Convert.ToDouble(Nodes[1].Visit());
-                        switch (data)
+                case (int)Operation.Operator:
+                    double a = Convert.ToDouble(Nodes[0].Visit());
+                    double b = Convert.ToDouble(Nodes[1].Visit());
+                    switch (data)
+                    {
+                        case "+":
+                            return (a + b).ToString();
+
+                        case "-":
+                            return (a - b).ToString();
+
+                        case "/":
+                            return (a / b).ToString();
+
+                        case "*":
+                            return (a * b).ToString();
+
+                        default:
+                            return (Math.Pow(a, b)).ToString();
+                    }
+                case (int)Operation.Setter:
+                    string s1 = Nodes[0].Visit();
+                    string s2 = Nodes[1].Visit();
+                    if (Double.TryParse(s2, out double x))
+                    {
+                        hashtable.Add(s1, s2);
+                        return "";
+                    }
+                    else if (hashtable.ContainsKey(s2))
+                    {
+                        hashtable.Add(s1, hashtable[s2]);
+                    }
+                    else
+                    {
+                        s2 = Program.CreatePostfixNotation(s2);
+                        Tree tempTree = new Tree(hashtable);
+                        tempTree.topNode.BuildTree(s2);
+                        hashtable.Add(s1, tempTree.Calculate());
+                    }
+                    return "";
+                case (int)Operation.Empty:
+
+                    for (int i = 0; i < Nodes.Count - 1; i++)
+                    {
+                        Nodes[i].Visit();
+                    }
+                    return Nodes[Nodes.Count - 1].Visit();
+                case (int)Operation.Data:
+                    {
+                        if (hashtable.ContainsKey(data))
                         {
-                            case "+":
-                                return (a + b).ToString();
-
-                            case "-":
-                                return (a - b).ToString();
-
-                            case "/":
-                                return (a / b).ToString();
-
-                            case "*":
-                                return (a * b).ToString();
-
-                            default:
-                                return (Math.Pow(a, b)).ToString();
-                        }
-                    case (int)Operation.Setter:
-                        string s = Nodes[0].Visit();
-                        if (hashtable.ContainsKey(s))
-                        {
-                            hashtable[s] = Nodes[1].Visit();
+                            return hashtable[data].ToString();
                         }
                         else
                         {
-                            hashtable.Add(s, Nodes[1].Visit());
+                            return data;
                         }
-                        return "";
-                    case (int)Operation.Empty:
-
-                        for (int i = 0; i < Nodes.Count - 1; i++)
-                        {
-                            Nodes[i].Visit();
-                        }
-                        return Nodes[Nodes.Count - 1].Visit();
-                    case (int)Operation.Data:
-                        {
-                            if (hashtable.ContainsKey(data))
-                            {
-                                return hashtable[data].ToString();
-                            }
-                            else
-                            {
-                                return data;
-                            }
-                        }
-                    default:
-                        return "";
-                }
-            }   
+                    }
+                default:
+                    return "";
+            }
+        }
 
         public void BuildTree(string problem)
         {
             Node pointer = this;
             string[] tokens = problem.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            for (int i = tokens.Length-1; i >= 0; i--)
+            for (int i = tokens.Length - 1; i >= 0; i--)
             {
                 switch (tokens[i])
                 {
-                    case "+": case "*": case "-": case "/": case "^":
+                    case "+":
+                    case "*":
+                    case "-":
+                    case "/":
+                    case "^":
                         Node op = new Node(hashtable, Operation.Operator, tokens[i], pointer);
                         if (pointer.parent != null)
                         {
@@ -105,7 +133,7 @@ namespace TreesLab
                         pointer = op;
                         break;
                     default:
-                        pointer.Nodes.Insert(0,new Node(hashtable, Operation.Data, tokens[i], pointer));
+                        pointer.Nodes.Insert(0, new Node(hashtable, Operation.Data, tokens[i], pointer));
                         break;
                 }
                 while (pointer.Nodes.Count > 1 && pointer.parent != null)
@@ -114,8 +142,8 @@ namespace TreesLab
                 }
             }
         }
-       
-        
-        }
-    
+
+
+    }
+
 }
